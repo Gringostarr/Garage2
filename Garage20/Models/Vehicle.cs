081a9b10e4eab8083608e598eec5a8094931b495
Tom Boye <mesolithicum@gmail.com>
2016-10-18 10:51:29 +0200

    Created new project

---
+++ Garage20/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage20.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Regnr { get; set; }
+        public string Color { get; set; }
+        [Display(Name = "Wheels")]
+        public int NumberOfWheels { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+    }
+
+    public enum VehicleType
+    {
+        Car,
+        Airplane,
+        Boat,
+        Bus,
+        Motorcycle
+    }
+}
---
+++ Garage20/Models/VehicleContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Garage20.Models
+{
+    public class VehicleContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
