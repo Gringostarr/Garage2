27511f05c929444b62e4b28198b078bf03ca50ca
Tom Boye <mesolithicum@gmail.com>
2016-10-19 14:46:02 +0200

    Added data validation for regnums and colors

--- Garage20/Models/Vehicle.cs
+++ Garage20/Models/Vehicle.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +11,21 @@ namespace Garage20.Models
     public class Vehicle
     {
         public int Id { get; set; }
+
+        [RegularExpression(@"[a-zA-Z]{3}(([0-9][0-9][1-9])|([1-9][0-9]0)|(0[1-9]0))", ErrorMessage ="Invalid registration number")]
         public string Regnr { get; set; }
+
+        [VehicleColor(ErrorMessage = "Unrecognized color")]
         public string Color { get; set; }
+
+        [Range(0, 100)]
         [Display(Name = "Wheels")]
         public int NumberOfWheels { get; set; }
+
         public VehicleType VehicleType { get; set; }
+
         public DateTime Checkin { get; set; }
+
         public DateTime Checkout { get; set; }
     }
 
@@ -25,5 +36,27 @@ namespace Garage20.Models
         Boat,
         Bus,
         Motorcycle
+    }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    sealed public class VehicleColor : ValidationAttribute
+    {
+        public VehicleColor()
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            string name = value as string;
+
+            Color color = Color.FromName(name);
+            return color.IsKnownColor;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return String.Format(CultureInfo.CurrentCulture, ErrorMessageString, name);
+        }
     }
 }
